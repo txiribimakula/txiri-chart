eb22945ee1d076bd249db94d2cc017092a94d3b9
txiribimakula <pablotobalina@gmail.com>
2021-08-29 18:33:30 +0200

    show txiri-chart on demo

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TxiriChartModule } from 'txiri-chart';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TxiriChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
